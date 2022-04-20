bb3507631b9c982a5ddab002d4683946720b34b4
le-huy-jh <74605855+yuhlee610@users.noreply.github.com>
2022-04-20 21:34:39 +0700

    Host image by firebase cloud storage

--- client/src/hook/useUpdateProfile.js
+++ client/src/hook/useUpdateProfile.js
@@ -1,6 +1,12 @@
 import { useState, useEffect } from "react";
 import { notify } from "../helper/notify";
 import moment from "moment";
+import {
+  getDownloadURL,
+  ref,
+  uploadString,
+} from "firebase/storage";
+import { storage } from "../config/firebase";
 
 export const useUpdateProfile = (
   form,
@@ -11,7 +17,7 @@ export const useUpdateProfile = (
   birthday,
   avatar,
   idUser,
-  onUpdateInfo,
+  onUpdateInfo
 ) => {
   const [imageBase64, setImageBase64] = useState("");
   const dateFormat = "YYYY/MM/DD";
@@ -24,7 +30,11 @@ export const useUpdateProfile = (
       address: (address !== "null" && address) || "",
       email: (email !== "null" && email) || "",
       phone: (phone !== "null" && phone) || "",
-      birthday: (birthday !== "null" && birthday !== null && moment(birthday, dateFormat)) || "",
+      birthday:
+        (birthday !== "null" &&
+          birthday !== null &&
+          moment(birthday, dateFormat)) ||
+        "",
     });
   }, []);
 
@@ -38,13 +48,20 @@ export const useUpdateProfile = (
       return;
     }
     setLoading(true);
-    fetch(`${process.env.REACT_APP_HOST_DOMAIN}/api/user/`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ ...values, avatar: imageBase64, id: idUser }),
-    })
+
+    // Data URL string
+    const storageRef = ref(storage, idUser + new Date().getTime());
+    uploadString(storageRef, imageBase64, "data_url")
+      .then((_) => getDownloadURL(storageRef))
+      .then((url) =>
+        fetch(`${process.env.REACT_APP_HOST_DOMAIN}/api/user/`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ ...values, avatar: url, id: idUser }),
+        })
+      )
       .then((response) => {
         if (response.ok) {
           notify(
@@ -65,8 +82,7 @@ export const useUpdateProfile = (
       })
       .then((response) => response.json())
       .then((result) => {
-        result.Avatar &&
-          setImageBase64(result.Avatar);
+        result.Avatar && setImageBase64(result.Avatar);
         onUpdateInfo(result);
       })
       .catch(() => {
@@ -77,12 +93,46 @@ export const useUpdateProfile = (
         );
         setLoading(false);
       });
+
+    // fetch(`${process.env.REACT_APP_HOST_DOMAIN}/api/user/`, {
+    //   method: "PUT",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ ...values, avatar: imageBase64, id: idUser }),
+    // })
+    //   .then((response) => {
+    //     if (response.ok) {
+    //       notify(
+    //         "UPDATE SUCCESS",
+    //         "You have already update your info.",
+    //         "success"
+    //       );
+    //       setLoading(false);
+    //       return fetch(
+    //         `${process.env.REACT_APP_HOST_DOMAIN}/api/User?id=${idUser}`,
+    //         {
+    //           method: "GET",
+    //         }
+    //       );
+    //     } else {
+    //       throw Error;
+    //     }
+    //   })
+    //   .then((response) => response.json())
+    //   .then((result) => {
+    //     result.Avatar && setImageBase64(result.Avatar);
+    //     onUpdateInfo(result);
+    //   })
+    //   .catch(() => {
+    //     notify(
+    //       "LOAD FAILED",
+    //       "Something went wrong :( Please try again.",
+    //       "error"
+    //     );
+    //     setLoading(false);
+    //   });
   };
 
-  return [
-    updateAccount,
-    loading,
-    imageBase64,
-    setImageBase64,
-  ];
+  return [updateAccount, loading, imageBase64, setImageBase64];
 };
