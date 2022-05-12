ec2fdb785acb7dfc6601fb42c99c198990c07cb5
Nguyễn Đức Hòa <72092427+Nguyen-Duc-Hoa@users.noreply.github.com>
2022-05-12 20:55:22 +0700

    modifi userTracking

--- WatchWebsite_TLCN/WatchWebsite_TLCN/Controllers/UserTrackingController.cs
+++ WatchWebsite_TLCN/WatchWebsite_TLCN/Controllers/UserTrackingController.cs
@@ -28,23 +28,59 @@ namespace WatchWebsite_TLCN.Controllers
         }
 
         [HttpPost]
-        public async Task <ActionResult<UserTracking>> AddEditUserTracking(UserTracking model)
+        public async Task<ActionResult<tmpUserTracking>> AddEditUserTracking(tmpUserTracking model)
         {
             try
             {
-                UserTracking userTrac = await _unitOfWork.UserTrackings.Get(p => p.Cookie == model.Cookie && p.ProductId == model.ProductId);
-                if (userTrac == null)
+                UserTracking newTrac = new UserTracking()
                 {
-                    await _unitOfWork.UserTrackings.Insert(model);
+                    Cookie = model.Cookie,
+                    ProductId = model.ProductId,
+                    ClickCart = 0,
+                    ClickDetail = 0,
+                    Order = 0,
+                    Time =0
+
+                };
+
+                UserTracking userTrac = await _unitOfWork.UserTrackings.Get(p => p.Cookie == model.Cookie && p.ProductId == model.ProductId);
+                if (userTrac != null)
+                {
+                    newTrac = userTrac;
+                    //await _unitOfWork.UserTrackings.Insert(model);
+                }
+                //else
+                //{
+                //    userTrac.Order += model.Order;
+                //    userTrac.Time += model.Time;
+                //    userTrac.ClickCart += model.ClickCart;
+                //    userTrac.ClickDetail += model.ClickDetail;
+
+                //    _unitOfWork.UserTrackings.Update(userTrac);
+                //}
+
+                switch (model.Behavior)
+                {
+                    case "Order":
+                        newTrac.Order += 1;
+                        break;
+                    case "ClickDetail":
+                        newTrac.ClickDetail += 1;
+                        break;
+                    case "ClickCart":
+                        newTrac.ClickCart += 1;
+                        break;
+                    default:
+                        break;
+                }
+
+                if(userTrac == null)
+                {
+                    await _unitOfWork.UserTrackings.Insert(newTrac);
                 }
                 else
                 {
-                    userTrac.Order += model.Order;
-                    userTrac.Time += model.Time;
-                    userTrac.ClickCart += model.ClickCart;
-                    userTrac.ClickDetail += model.ClickDetail;
-
-                    _unitOfWork.UserTrackings.Update(userTrac);
+                    _unitOfWork.UserTrackings.Update(newTrac);
                 }
                 await _unitOfWork.Save();
             }
@@ -56,5 +92,13 @@ namespace WatchWebsite_TLCN.Controllers
 
 
         }
+
+    }
+
+    public class tmpUserTracking
+    {
+        public string Cookie { get; set; }
+        public string ProductId { get; set; }
+        public string Behavior { get; set; }
     }
 }
