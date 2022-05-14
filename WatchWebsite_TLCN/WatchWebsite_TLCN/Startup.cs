b57afcef0f23ee8902fea035439e7ec8316df7a6
le-huy-jh <yuhlee610@gmail.com>
2022-05-14 20:47:46 +0700

    Auto udpate dataset after 24 hour

--- WatchWebsite_TLCN/WatchWebsite_TLCN/Startup.cs
+++ WatchWebsite_TLCN/WatchWebsite_TLCN/Startup.cs
@@ -38,6 +38,8 @@ namespace WatchWebsite_TLCN
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHostedService<UpdateDatasetBackgroundServcie>();
+
             // Đăng ký UnitOfWork
             services.AddTransient<IUnitOfWork, UnitOfWork>();
 
---
+++ WatchWebsite_TLCN/WatchWebsite_TLCN/UpdateDatasetBackgroundServcie.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WatchWebsite_TLCN
+{
+    public class UpdateDatasetBackgroundServcie : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            int refreshTimeMs = 86400000; //24 hour
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                //call your function here
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    string domainName = "https://kltn-watchwebsite.herokuapp.com/";
+                    await httpClient.GetAsync($"{domainName}/api/products/sendProductUser");
+                }
+
+                await Task.Delay(refreshTimeMs, stoppingToken);
+            }
+        }
+    }
+}
